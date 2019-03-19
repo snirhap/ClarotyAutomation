f42803eca1bfb02c2267390a964e00cd44e5ff18
Snir <snirhap@gmail.com>
2019-03-19 21:05:17 +0200

    Added GoogleResults class that provides different stats after getting google search

--- google_searcher.py
+++ google_searcher.py
@@ -1,50 +1,23 @@
 from selenium.webdriver.common.keys import Keys
-import re
 
 
 class GoogleSearcher:
-    def __init__(self, browser_handler):
+    def __init__(self, search_handler):
         """
             expect to get an instance of Selenium web driver (e.g. Chrome, Firefox, etc.)
         """
-        self.results_list = []
-        self.browser_handler = browser_handler
+
+        self.search_handler = search_handler
 
     def search(self, search_string):
         """
             open a desired browser and execute a search according to the search string input
+            returns a search handler that contains all data from the search query results
         """
-        self.browser_handler.get("https://www.google.com")
-        search_bar = self.browser_handler.find_element_by_xpath("//input[@name='q']")  # get the search bar element
+        self.search_handler.get("https://www.google.com")
+        search_bar = self.search_handler.find_element_by_xpath("//input[@name='q']")  # get the search bar element
         search_bar.send_keys(search_string)  # type in the search bar
         search_bar.send_keys(Keys.ENTER)  # generates an Enter key pressing
 
-    def get_number_of_results(self):
-        """
-            returns the number of results to the search string in Google
-        """
-        try:
-            stats = self.browser_handler.find_element_by_xpath("//div[@id='resultStats']").text
-            return int(''.join(elem for elem in re.findall(r'\d*', re.sub(r'\(.*?\)', '', stats)) if elem.isdigit()))
-        except:
-            raise Exception('No search was executed')
+        return self.search_handler
 
-    def get_result_link_by_position(self, position=1):
-        """
-            Returns the URL that appears in the position provided
-            Default is the first result in Google
-            position = 1 will return the first result in Google search
-        """
-        try:
-            all_results = self.browser_handler.find_elements_by_xpath("//div[@id='search']//div[@class='g']")
-
-            for result in all_results:
-                url = result.find_element_by_tag_name('a').find_element_by_tag_name('cite').find_element_by_tag_name('span').text
-                self.results_list.append(url)
-
-            return self.results_list[position-1]
-
-        except IndexError:
-            raise Exception('Position provided is invalid')
-        except:
-            raise Exception('No search was executed')
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from google_searcher import GoogleSearcher
+from google_results import GoogleResults
 from claroty_careers_searcher import ClarotyCareerSearcher
 
 if __name__ == '__main__':
@@ -8,18 +9,20 @@ if __name__ == '__main__':
 
     # Google search
     google_searcher = GoogleSearcher(chrome_browser)
-    google_searcher.search('Claroty')
+    google_search_results_handler = google_searcher.search('Claroty')
+
+    google_search_results = GoogleResults(google_search_results_handler)
 
     # Get the number of results in Google
-    print('Number of results: {}\n'.format(google_searcher.get_number_of_results()))
+    print('Number of results: {}\n'.format(google_search_results.get_number_of_results()))
 
     # Check if Calroty is the first result link in the result
-    first_result_link = google_searcher.get_result_link_by_position()  # 1 = first result
+    first_result_link = google_search_results.get_result_link_by_position()  # 1 = first result
 
     if first_result_link in ['https://www.claroty.com/', 'http://www.claroty.com/']:
         print('https://www.claroty.com/ is the first result link\n')
     else:
-        print('{} is the first result link\n'.format(first_result_link))
+        print('https://www.claroty.com/ is not the first result link, {} is the first result link\n'.format(first_result_link))
 
     # Claroty career search
     # Check the number of openings in Claroty
