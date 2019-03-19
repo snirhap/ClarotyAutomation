a5c04ed90f4406171b4580cde3e69679e2e44507
Snir <snirhap@gmail.com>
2019-03-19 19:38:58 +0200

    Updated google_searcher.get_first_result function to get_result_link_by_position, so if, for example would like to get the second result, we can do it.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     print('Number of results: {}\n'.format(google_searcher.get_number_of_results()))
 
     # Check if Calroty is the first result link in the result
-    first_result_link = google_searcher.get_result_link_by_position(1)  # 1 = first result
+    first_result_link = google_searcher.get_result_link_by_position()  # 1 = first result
 
     if first_result_link in ['https://www.claroty.com/', 'http://www.claroty.com/']:
         print('https://www.claroty.com/ is the first result link\n')
@@ -24,6 +24,7 @@ if __name__ == '__main__':
     # Claroty career search
     # Check the number of openings in Claroty
     claroty_searcher = ClarotyCareerSearcher(chrome_browser)
-    print('Current Number of Openings: {}\n'.format(claroty_searcher.get_number_of_openings()))
+    number_of_openings = claroty_searcher.get_number_of_openings()
+    print('Current Number of Openings: {}\n'.format(number_of_openings))
 
     chrome_browser.quit()
