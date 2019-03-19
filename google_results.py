8b28d23c4da0e457447af80e00e212977aa582a7
Snir <snirhap@gmail.com>
2019-03-19 23:42:46 +0200

    Added GoogleResults class that provides different stats after getting google search

---
+++ google_results.py
@@ -0,0 +1,39 @@
+import re
+
+
+class GoogleResults:
+    def __init__(self, results_handler):
+        self.results_list = []
+        self.results_handler = results_handler
+
+    def get_number_of_results(self):
+        """
+            returns the number of results to the search string in Google
+        """
+        try:
+            stats = self.results_handler.find_element_by_xpath("//div[@id='resultStats']").text
+            return int(''.join(elem for elem in re.findall(r'\d*', re.sub(r'\(.*?\)', '', stats)) if elem.isdigit()))
+        except:
+            raise Exception('No search was executed')
+
+    def get_result_link_by_position(self, position):
+        """
+            Returns the URL that appears in the position provided
+            Default is the first result in Google
+            position = 1 will return the first result in Google search
+        """
+        try:
+            all_results = self.results_handler.find_elements_by_xpath("//div[@id='search']//div[@class='g']")
+
+            for result in all_results:
+                url = result.find_element_by_tag_name('a').find_element_by_tag_name('cite').find_element_by_tag_name('span').text
+                self.results_list.append(url)
+
+            return self.results_list[position-1]
+
+        except TypeError:
+            raise Exception('Missing position parameter')
+        except IndexError:
+            raise Exception('Position provided is invalid')
+        except:
+            raise Exception('No search was executed')
